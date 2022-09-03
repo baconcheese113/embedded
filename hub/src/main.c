e913abb556e3ea346407cf465d7cf262acd2d70c
baconcheese113 <baconcheese113@hotmail.com>
2022-09-02 20:59:40 -0600

    Port over RGB LED writing and happy_dance

--- hub/src/main.c
+++ hub/src/main.c
@@ -1,11 +1,29 @@
 #include <zephyr.h>
+#include <logging/log.h>
+LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
+
+#include "utilities.h"
 
 void main(void)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
-  
-	// 
-	// TODO get interrupt for pair button
+  LOG_INF("Booting...");
+  LOG_INF("Board: %s", CONFIG_BOARD);
+  Utilities.setup_pins();
 
-	// 
+  Utilities.happy_dance();
+  Utilities.rgb_write(0, 0, 0, true);
+
+  // TODO Start UART1 connection to SIM7000
+
+  // TODO network.setPower(true);
+
+  // TODO network.waitForConnection();
+
+
+  while (1) {
+
+    k_msleep(10);
+  }
+
+  // TODO get interrupt for pair button
 }
---
+++ hub/src/utilities.h
@@ -0,0 +1,35 @@
+#ifndef HUB_UTILITIES_H
+#define HUB_UTILITIES_H
+
+// struct Command {
+//   char type[30];
+//   char value[50];
+// };
+
+struct utilities {
+  /**
+   * Single place to register all used I/O pins
+   */
+  int (*setup_pins)(void);
+  /**
+   * Writes to the RGB pins the RGB values from 0 - 255
+  **/
+  void (*rgb_write)(uint8_t r, uint8_t g, uint8_t b, bool print);
+  void (*happy_dance)(void);
+};
+
+/**
+ * Parses BLE char arrays separated by a colon ( : ) delimeter into a Command struct
+ * Prints an error message if unable to parse
+**/
+// Command parseRawCommand(char* rawCmd);
+
+/**
+ * Reads n bytes into buffer (ignoring head) from Serial1
+ * Returns true if OK received, false otherwise
+**/
+// bool readUntilResp(const char* head, char* buffer, uint16_t timeout = 1000);
+
+extern const struct utilities Utilities;
+
+#endif
